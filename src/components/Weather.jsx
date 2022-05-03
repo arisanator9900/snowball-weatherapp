1f8e997e3cda187b1ffb60121802aeaac00675fd
arisanator9900 <96804792+arisanator9900@users.noreply.github.com>
2022-05-03 04:52:34 -0400

    updated weather component

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,17 +1,35 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Weather = ({Location}) => {
+
+    //use state hook to save weatherData into a variable to use below
+    const [currentWeather, setWeather] = useState('London')
+    //initialize the variables for the cards. (temperature, Clouds, rain and wind)
+    let [temperature, feelsLike, condition, icon, rain] = ['-','-','-','-','-'];
     //api call
     //only call when user presses button to get weather 
     let locationName = {Location}.Location
-
-    const getWeather = () =>{
+    locationName = locationName.replace(' ', '-')
+    const getWeather = () => {
         fetch('http://api.weatherapi.com/v1/current.json?key=d7c8dbc661c344ed9cb122953220205&q='+ locationName +'&aqi=no')
-
+        .then(response => {return response.json()})
+        .then(data => {setWeather(data)})
+        console.log(currentWeather)
+    }
+    //inital state will return undefined. If typeof isnt an object nothing will happen
+    if (typeof currentWeather == 'object') {
+        temperature = currentWeather.current.temp_f;
+        feelsLike = currentWeather.current.feelslike_f
+        condition = currentWeather.current.condition.text;
+        icon = 'https:' + currentWeather.current.condition.icon
+        rain = currentWeather.current.precip_in
     }
     
+
   return (
     <div className='wrapper'>
+    <p>{temperature}, {feelsLike}, {condition}, <img src={icon} alt=''/>,  {rain}</p>
         <div className="button">
             <button className='weather-btn' onClick={getWeather}>Get Weather</button>
         </div>
